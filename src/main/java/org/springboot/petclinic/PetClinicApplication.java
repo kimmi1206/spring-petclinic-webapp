7d101ec743fc2404da8501c9974d43b328c96e99
Kheiner Murillo <kheinerimm1206@gmail.com>
2022-09-20 19:32:06 -0500

    Initial Project Add

---
+++ src/main/java/org/springboot/petclinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
