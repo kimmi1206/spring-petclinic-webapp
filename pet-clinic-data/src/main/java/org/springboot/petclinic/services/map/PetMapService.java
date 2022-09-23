bf7e3ed571e1ccf46f37bd065f2a36b73fcdde5a
kimmi1206 <kheinerimm1206@gmail.com>
2022-09-22 21:04:41 -0500

    Impl Map based Services. closes #6
    Signed-off-by: kimmi1206 <kheinerimm1206@gmail.com>

---
+++ pet-clinic-data/src/main/java/org/springboot/petclinic/services/map/PetMapService.java
@@ -0,0 +1,34 @@
+package org.springboot.petclinic.services.map;
+
+import org.springboot.petclinic.model.Pet;
+import org.springboot.petclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
