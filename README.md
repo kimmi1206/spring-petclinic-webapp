22126c455347acca5ca9fb673a86d9f548fef706
kimmi1206 <kheinerimm1206@gmail.com>
2022-09-29 19:20:08 -0500

    Fixed Error in display Owner #61
    Signed-off-by: kimmi1206 <kheinerimm1206@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-petclinic-webapp
 Spring Boot Pet Clinic
+
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/kimmi1206/spring-petclinic-webapp/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/kimmi1206/spring-petclinic-webapp/tree/main)
+
---
+++ pet-clinic-web/src/test/java/org/springboot/petclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,77 @@
+package org.springboot.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springboot.petclinic.model.Owner;
+import org.springboot.petclinic.services.OwnerService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    Set<Owner> owners;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1l).build());
+        owners.add(Owner.builder().id(2l).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+
+        Mockito.verify(ownerService, Mockito.atLeast(1)).findAll();
+
+    }
+
+    /*
+    @Test
+    void findOwners() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
+
+        // Mockito.verify(ownerService, Mockito.times(0)).findAll();
+        Mockito.verify(ownerService, Mockito.atLeast(1)).findAll();
+
+        // Mockito.verify(MOCKED_OBJ, Mockito.times(number)).YOUR_METHOD_CALL();
+        // Mockito.verify(MOCKED_OBJ, Mockito.atLeast(number)).YOUR_METHOD_CALL();
+        // verifyNoMoreInteractions(ownerService);
+    }
+    */
+
+}
